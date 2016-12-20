cba13cc0a8979f7099a61f1736a6cf5152cb4e49
Erik Teichmann <erik-teichmann@gmx.de>
2016-12-20 12:05:38 +0100

    Add documentation and slight refactoring for interpretation

--- simulations/js/interpretation.js
+++ simulations/js/interpretation.js
@@ -1,15 +1,23 @@
+// Sum all the elements in an array
 function sumArray(data) {
+
   var sum = 0;
+
   for (var i = 0; i < data.length; i++) {
     sum += data[i];
   }
+
   return sum;
 }
 
 
+// Returns the steplength between a given number (consolidate) of collisions.
+// There seems to be a small bug, where we sometimes get steps for an edge-case.
 function prepDelta() {
+
   var delta = [[], []];
   var data = [[], []]
+
   for (var i = 0; i < particle.length; i++) {
     for (var j = 0; j < particle[i].history.length / 2 - 1; j++) {
       var diffX = particle[i].history[2 * (j + 1)] - particle[i].history[2 * j];
@@ -19,7 +27,11 @@ function prepDelta() {
       delta[1].push(diffY);
     }
   }
+
+  // delta is an array of single steps, but we want to sum over a given number
+  // of steps.
   var len = delta[0].length;
+
   for (var i = 0; i < len / consolidate; i++){
     var dat = sumArray(delta[0].slice(0, consolidate));
     data[0].push(dat);
@@ -28,16 +40,22 @@ function prepDelta() {
     delta[0].splice(0, consolidate);
     delta[1].splice(0, consolidate);
   }
+
   return data;
 }
 
 
+// Builds a histogram of all the delta. We can build the x-range easily, since
+// all deltas are integers.
 function buildHistogram(delta) {
+
   var data = [[], []]
+
   for (var i = 0; i < 2*consolidate + 1; i++) {
     data[0].push(-consolidate + i);
     data[1].push(0);
   }
+
   for (var i = 0; i < delta.length; i++) {
     for (var j = 0; j < data[0].length; j++) {
       if (delta[i] == data[0][j]) {
@@ -45,38 +63,52 @@ function buildHistogram(delta) {
       }
     }
   }
+
   return data;
 }
 
 
+// Calculates the mean.
 function calcMean(data) {
+
   var mean = 0;
+
   for (var i = 0; i < data.length; i++) {
     mean += data[i];
   }
+
   return mean/data.length;
 }
 
 
+// Calculates the Variance.
 function calcVariance(data) {
+
   var mean = calcMean(data);
   var vari = 0;
+
   for (var i = 0; i < data.length; i++) {
     vari += (data[i] - mean)*(data[i] - mean);
   }
+
   return vari/data.length;
 }
 
 
+// Calculates the standard deviation using the variance.
 function calcStdev(data) {
   var vari = calcVariance(data);
   return Math.sqrt(vari);
 }
 
 
+// Returns the y-values for a normal distribution for every integer, since
+// steps are always integers and the number of steps suffices for a smooth
+// distribution.
+// y = 1/sqrt(2*pi*var)*exp(- (x-mean)**2/(2*var))
 function normalDistribution(mean, vari) {
-  var data = [];
 
+  var data = [];
 
   for (var k = 0; k < 2*consolidate + 1; k++) {
     var i = -consolidate + k
@@ -84,5 +116,6 @@ function normalDistribution(mean, vari) {
           Math.exp((-1) * (i - mean) * (i - mean) / (2 * vari));
     data.push(exp);
   }
+
   return data;
 }
