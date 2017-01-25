d9bf3c92113428f9d66c8dfc4f5276e4dd36b6e3
Erik Teichmann <erik-teichmann@gmx.de>
2017-01-25 11:34:27 +0100

    Remove color from plot

--- tex/figures/histogram.py
+++ tex/figures/histogram.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams["figure.figsize"] = [8, 5]
+plt.rcParams['font.size'] = 20
+
 mu, sigma = 0., 1.
 data = np.random.normal(mu, sigma, 10000)
-count, bins, ignored = plt.hist(data, 50, normed=True)
+count, bins, ignored = plt.hist(data, 50, normed=True, linestyle='solid', color='white', label='Histogram')
 plt.xlabel(r'$\Delta x$')
 plt.ylabel(r'Wahrscheinlichkeit')
 plt.plot(bins, 1/(sigma*np.sqrt(2*np.pi))*np.exp(-(bins - mu)**2/(2*sigma**2)),
-         linewidth=2, color='r')
+         linewidth=2, linestyle='dashed', color='black', label='Normalverteilung')
 plt.xlim(-4., 4.)
-plt.savefig('histogram.pdf', dpi=600)
+lgd = plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2,
+                mode="expand", borderaxespad=0., fontsize="13")
+plt.tight_layout()
+plt.savefig('histogram.pdf', bbox_extra_artists=(lgd,), dpi=300)
